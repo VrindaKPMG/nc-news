74c7846dbd93d91dc58c3b13459611454a4586d4
VrindaKPMG <vrinda.gera@kpmg.co.uk>
2023-01-10 16:22:07 +0000

    added alt message for no comments situation and changed css styles for smaller screens - links will need changing but in the future

--- src/components/ViewComments.jsx
+++ src/components/ViewComments.jsx
@@ -21,17 +21,29 @@ const ViewComments = () => {
 
     if (isLoading) return <p>Gossip incoming....</p>
 
+ 
+
+    if (comments.length === 0) {
+        return (
+            <h3>It's very quiet here</h3>
+        ) 
+    } else {
+
+    }
+
     return (
         <main>
             <h3>Comments</h3> 
             <Link to={`/articles/${article_id}`}>⬅️Back to article</Link> <br /> <br /> <br />
-            {comments.map((comment) => <p>
+            {comments.map((comment) => <article key={comment.comment_id}>
                 <strong>{comment.author}</strong> says...
                  <div className="comments_body">{comment.body}</div>
                  <button>❤ {comment.votes}</button> <br /> <br />
-            </p>)}
+            </article>)}
         </main>
     )
+
+    
 
 };
 
